6dfbc7b6285670167fdbdc2fdea12adc23c67b16
Ricardo S. Chao <richiechao95@gmail.com>
2020-09-22 17:48:23 +0800

    [new] add update script

--- update.sh
+++ update.sh
@@ -6,10 +6,6 @@
 git fetch --all
 git reset --hard origin/master
 
-# grep column_list from configuration.py
-# head -n 99 configuration.py | tail -n 46 > column_list.txt
-python configuration.py
-
 echo "The repository has been successfully updated.
 The script now closing"
 
